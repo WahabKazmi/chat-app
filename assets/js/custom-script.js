7ae7f0eaedb866b0847e1731c72ce318393650ed
Wahab Kazmi <wahab.kazmi2000@gmail.com>
2024-05-12 08:49:14 -0700

    main folder structure changed

---
+++ assets/js/custom-script.js
@@ -0,0 +1,7 @@
+export const redirection = (path) => {
+    // if (!window.location.href.includes('netlify')) {
+    //     window.location.replace('/dist/'+path)
+    // } else {
+    // }
+    window.location.replace('/'+path)
+}
--- dist/assets/js/custom-script.js
+++
@@ -1,7 +0,0 @@
-export const redirection = (path) => {
-    if (!window.location.href.includes('netlify')) {
-        window.location.replace('/dist/'+path)
-    } else {
-        window.location.replace('/'+path)
-    }
-}
