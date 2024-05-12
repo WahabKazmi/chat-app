913ad675dfc044113e2db30e4edc24a7863ab316
Wahab Kazmi <wahab.kazmi2000@gmail.com>
2024-05-12 08:53:47 -0700

    github error fix

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -1,7 +1,7 @@
 export const redirection = (path) => {
-    // if (!window.location.href.includes('netlify')) {
-    //     window.location.replace('/dist/'+path)
-    // } else {
-    // }
-    window.location.replace('/'+path)
+    if (!window.location.href.includes('github')) {
+        window.location.replace('/'+path)
+    } else {
+        window.location.replace('/chat-app/'+path)
+    }
 }
